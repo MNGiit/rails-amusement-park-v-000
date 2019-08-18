c75b7bdd49334fbba2bd2e96a03ee93b3a77de21
Mohammed Noori <kmanoorib@gmail.com>
2019-08-18 03:17:49 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,21 +2,35 @@ class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
 
-  def take_ride # can make it neater and remove self.
-    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
-      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
-    elsif self.user.tickets < self.attraction.tickets
+  def take_ride
+    if not_enough_tickets && !not_tall_enough
       "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
-    elsif self.user.height < self.attraction.min_height
+    elsif not_tall_enough && !not_enough_tickets
       "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    elsif not_enough_tickets && not_tall_enough
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
     else
-      self.user.nausea += self.attraction.nausea_rating
-      self.user.happiness += self.attraction.happiness_rating
-      self.user.tickets -= self.attraction.tickets
-
-      self.user.save # important to save so it can reflect what happened
-      "Thanks for riding the #{attraction.name}!"
+      get_on_ride
     end
   end
 
+  def get_on_ride
+    new_happiness = self.user.happiness + self.attraction.happiness_rating
+    new_nausea = self.user.nausea + self.attraction.nausea_rating
+    new_tickets_number = self.user.tickets - self.attraction.tickets
+
+    self.user.update(
+      happiness: new_happiness, 
+      nausea: new_nausea, 
+      tickets: new_tickets_number)
+      "Thanks for riding the #{attraction.name}!"
+  end
+
+  def not_enough_tickets
+    self.user.tickets < self.attraction.tickets
+  end
+
+  def not_tall_enough
+    self.user.height < self.attraction.min_height
+  end
 end
