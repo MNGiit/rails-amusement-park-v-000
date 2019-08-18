e100f800358edfbb4852b95b661f4af45151c12e
Mohammed Noori <kmanoorib@gmail.com>
2019-08-18 02:21:21 +0000

    Automatically backed up by Learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  skip_before_action :user_verified, only: [:signin, :create]
+
+  def signin
+    @user = User.new
+  end
+
+  def create
+    if @user = User.find_by(name: params[:user][:name])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to '/signin'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#index'
+
+  resources :attractions
+  resources :sessions
+  resources :users
+
+  # get '/signin', to: 'application#signin', as: 'signin'
+  get '/signin', to: 'sessions#signin', as: 'signin'
+  # post   '/login',   to: 'sessions#create'
+  post '/signin', to: 'sessions#create', as: 'create'
+  delete '/signout', to: 'sessions#destroy', as: 'destroy'
 end
