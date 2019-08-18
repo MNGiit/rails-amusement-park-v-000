5969b4ac783cb3f77654c845a13fec300f4c22ed
Mohammed Noori <kmanoorib@gmail.com>
2019-08-18 03:18:49 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,36 +1,45 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
-
   def take_ride
-    if not_enough_tickets && !not_tall_enough
-      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
-    elsif not_tall_enough && !not_enough_tickets
-      "Sorry. You are not tall enough to ride the #{self.attraction.name}."
-    elsif not_enough_tickets && not_tall_enough
-      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
-    else
-      get_on_ride
-    end
-  end
+          if user_is_tall_enough? && user_has_enough_tickets?
+              start_ride
+          elsif !user_is_tall_enough? && user_has_enough_tickets?
+              sorry_period + not_tall_enough
+          elsif user_is_tall_enough? && !user_has_enough_tickets?
+              sorry_period + not_enough_tickets
+          else
+              sorry_period + not_enough_tickets + " " + not_tall_enough
+          end
+      end
 
-  def get_on_ride
-    new_happiness = self.user.happiness + self.attraction.happiness_rating
-    new_nausea = self.user.nausea + self.attraction.nausea_rating
-    new_tickets_number = self.user.tickets - self.attraction.tickets
+      private
 
-    self.user.update(
-      happiness: new_happiness,
-      nausea: new_nausea,
-      tickets: new_tickets_number)
-      "Thanks for riding the #{attraction.name}!"
-  end
+      def sorry_period
+          "Sorry. "
+      end
 
-  def not_enough_tickets
-    self.user.tickets < self.attraction.tickets
-  end
+      def user_is_tall_enough?
+          user.height >= attraction.min_height ? true : false
+      end
 
-  def not_tall_enough
-    self.user.height < self.attraction.min_height
+      def user_has_enough_tickets?
+          user.tickets >= attraction.tickets ? true : false
+      end
+
+      def not_enough_tickets
+          "You do not have enough tickets to ride the #{self.attraction.name}."
+      end
+
+      def not_tall_enough
+          "You are not tall enough to ride the #{attraction.name}."
+      end
+
+      def start_ride
+          user.nausea = user.nausea + attraction.nausea_rating
+          user.happiness = user.happiness + attraction.happiness_rating
+          user.tickets = user.tickets - attraction.tickets
+          user.save
+          "Thanks for riding the #{attraction.name}!"
+      end
   end
-end
